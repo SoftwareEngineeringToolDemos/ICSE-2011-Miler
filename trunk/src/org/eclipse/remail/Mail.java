6fe9e6deec99c5bdd82f1c86304597cbe8ea3559
baracchi.lorenzo@gmail.com <baracchi.lorenzo@gmail.com@8272bd5c-ea74-4d78-a600-467abb2fde90>
2011-07-13 08:46:23 +0000

    Added a new faster method to merge two mail list Deprecated the old inefficient merge
    git-svn-id: svn://localhost/tmp/testgc@40 8272bd5c-ea74-4d78-a600-467abb2fde90

--- trunk/src/org/eclipse/remail/Mail.java
+++ trunk/src/org/eclipse/remail/Mail.java
@@ -1,7 +1,9 @@
-package org.eclipse.remail; 
+package org.eclipse.remail;
 
+import java.util.Collections;
 import java.util.Date;
 import java.util.LinkedList;
+import java.util.NoSuchElementException;
 
 /**
  * Data class implementing the data structure of a single email. Email content
@@ -11,8 +13,7 @@ import java.util.LinkedList;
  * 
  * @author V. Humpa
  */
-public class Mail implements Comparable
-{
+public class Mail implements Comparable {
 	private int id;
 	private String subject;
 	private String author;
@@ -22,17 +23,15 @@ public class Mail implements Comparable
 	private String classname; // name of the class searched
 	private String text;
 
-	public Mail(int id, String subject, Date timestamp, String classname)
-	{
+	public Mail(int id, String subject, Date timestamp, String classname) {
 		this.setId(id);
 		this.setSubject(subject);
 		this.setTimestamp(timestamp);
 		this.setClassname(classname);
 	}
 
-	public Mail(int id, String subject, Date timestamp, String author,
-			String permalink, String threadlink, String text, String classname)
-	{
+	public Mail(int id, String subject, Date timestamp, String author, String permalink,
+			String threadlink, String text, String classname) {
 		this.setId(id);
 		this.setSubject(subject);
 		this.setTimestamp(timestamp);
@@ -43,83 +42,67 @@ public class Mail implements Comparable
 		this.setClassname(classname);
 	}
 
-	public void setId(int id)
-	{
+	public void setId(int id) {
 		this.id = id;
 	}
 
-	public int getId()
-	{
+	public int getId() {
 		return id;
 	}
 
-	public void setSubject(String subject)
-	{
+	public void setSubject(String subject) {
 		this.subject = subject;
 	}
 
-	public String getSubject()
-	{
+	public String getSubject() {
 		return subject;
 	}
 
-	public void setTimestamp(Date timestamp)
-	{
+	public void setTimestamp(Date timestamp) {
 		this.timestamp = timestamp;
 	}
 
-	public Date getTimestamp()
-	{
+	public Date getTimestamp() {
 		return timestamp;
 	}
 
-	public void setClassname(String classname)
-	{
+	public void setClassname(String classname) {
 		this.classname = classname;
 	}
 
-	public String getClassname()
-	{
+	public String getClassname() {
 		return classname;
 	}
 
-	public void setAuthor(String author)
-	{
+	public void setAuthor(String author) {
 		this.author = author;
 	}
 
-	public String getAuthor()
-	{
+	public String getAuthor() {
 		return author;
 	}
 
-	public void setPermalink(String permalink)
-	{
+	public void setPermalink(String permalink) {
 		this.permalink = permalink;
 	}
 
-	public String getPermalink()
-	{
+	public String getPermalink() {
 		return permalink;
 	}
 
-	public void setThreadlink(String threadlink)
-	{
+	public void setThreadlink(String threadlink) {
 		this.threadlink = threadlink;
 	}
 
-	public String getThreadlink()
-	{
+	public String getThreadlink() {
 		return threadlink;
 	}
 
-	public void setText(String text)
-	{
+	public void setText(String text) {
 		this.text = text;
 	}
 
-	public String getText()
-	{
+	public String getText() {
 		return text;
 	}
 
@@ -127,8 +110,7 @@ public class Mail implements Comparable
 	 * Implements mandatory method from Comparable, allows for <,> operation
 	 */
 	@Override
-	public int compareTo(Object o)
-	{
+	public int compareTo(Object o) {
 		Mail mail = (Mail) o;
 		if (this.getTimestamp().before(mail.getTimestamp()))
 			return -1;
@@ -138,32 +120,114 @@ public class Mail implements Comparable
 	}
 
 	/**
-	 * Merges two lists of Mail into one, intersection compared by permalink values
+	 * @deprecated Inefficient, has complexity O(n). Replaced by
+	 *             {@link #mergeSortMailList(LinkedList, LinkedList)} Merges two
+	 *             lists of Mail into one, intersection compared by permalink
+	 *             values
 	 * @param mailList
 	 * @param mailList2
 	 * @return
 	 */
 	public static LinkedList<Mail> mergeMailLists(LinkedList<Mail> mailList,
-			LinkedList<Mail> mailList2)
-	{
+			LinkedList<Mail> mailList2) {
 		if (mailList == null || mailList2 == null)
 			return null;
 		LinkedList<Mail> mailList3 = new LinkedList<Mail>();
-		for (Mail m2 : mailList2)
-		{
+		for (Mail m2 : mailList2) {
 			boolean newmail = true;
-			for (Mail m1 : mailList)
-			{
-				if (m1.getPermalink().startsWith(m2.getPermalink()))
-				{
+			for (Mail m1 : mailList) {
+				if (m1.getPermalink().startsWith(m2.getPermalink())) {
 					newmail = false;
 					break;
 				}
 			}
-			if(newmail)
+			if (newmail)
 				mailList3.add(m2);
 		}
 		mailList.addAll(mailList3);
 		return mailList;
+	}
+
+	/**
+	 * Merges two lists of Mail into one, intersection compared by permalink
+	 * values. Complexity of this is n*log(n);
+	 * 
+	 * @param mailList1
+	 * @param mailList2
+	 * @return the merged list
+	 */
+	@SuppressWarnings("unchecked")
+	public static LinkedList<Mail> mergeSortMailList(LinkedList<Mail> mailList1,
+			LinkedList<Mail> mailList2) {
+		LinkedList<Mail> mergedList = new LinkedList<Mail>();
+
+		// sort the two collections
+		Collections.sort(mailList1);
+		Collections.sort(mailList2);
+
+		// merge them
+		boolean list1end = false;
+		boolean list2end = false;
+		while (!list1end && !list2end) {
+			Mail mailFromList1 = null;
+			Mail mailFromList2 = null;
+
+			try {
+				// get and remove the first element in the list
+				mailFromList1 = mailList1.removeFirst();
+			} catch (NoSuchElementException e) {
+				// list is empty
+				list1end = true;
+				mailFromList1 = null;
+			}
+			try {
+				// get and remove the first element in the list
+				mailFromList2 = mailList2.removeFirst();
+			} catch (NoSuchElementException e) {
+				// list is empty
+				list2end = true;
+				mailFromList2 = null;
+			}
+
+			// compare the two mail to insert in order
+			if (mailFromList1 == null && mailFromList2 != null)
+				mergedList.add(mailFromList2);
+			else if (mailFromList2 == null && mailFromList1 != null)
+				mergedList.add(mailFromList1);
+			else if (mailFromList1 != null && mailFromList2 != null) {
+				int compare = mailFromList1.compareTo(mailFromList2);
+				switch (compare) {
+					case -1: {
+						/*
+						 * The mail from List1 has to be inserted before mail
+						 * form List2
+						 */
+						mergedList.add(mailFromList1);
+						mergedList.add(mailFromList2);
+						break;
+					}
+					case 1: {
+						/*
+						 * The mail from List2 has to be inserted before mail
+						 * from List1
+						 */
+						mergedList.add(mailFromList2);
+						mergedList.add(mailFromList1);
+						break;
+					}
+					case 0: {
+						/*
+						 * The mail from List1 and List2 are the same. Insert
+						 * only one of them!
+						 */
+						mergedList.add(mailFromList1);
+						break;
+					}
+				}
+			}
+
+		}
+
+		return mergedList;
 	}
 }
