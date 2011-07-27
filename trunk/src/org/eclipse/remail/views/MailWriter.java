b3feb110ffc521f9f50b3561f820f7403f91d3b4
baracchi.lorenzo@gmail.com <baracchi.lorenzo@gmail.com@8272bd5c-ea74-4d78-a600-467abb2fde90>
2011-07-27 15:24:11 +0000

    Just added library to send emails
    git-svn-id: svn://localhost/tmp/testgc@69 8272bd5c-ea74-4d78-a600-467abb2fde90

---
+++ trunk/src/org/eclipse/remail/views/MailWriter.java
@@ -0,0 +1,27 @@
+package org.eclipse.remail.views;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+/**
+ * Class used to provide to the user a nice interface
+ * for writing e-mails
+ * 
+ * @author Lorenzo Baracchi <lorenzo.baracchi@usi.ch>
+ *
+ */
+public class MailWriter extends ViewPart {
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
